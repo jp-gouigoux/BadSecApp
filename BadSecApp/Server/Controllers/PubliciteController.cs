cf1726b974aafadefd0cb8adec30e6931b0ef93b
JP Gouigoux <jp.gouigoux@free.fr>
2021-12-11 16:26:08 +0100

    Correction de l'appel de publicité qui était réalisé directement par le client, alors que sa place est côté serveur pour éviter les problèmes de CORS et éviter de fuiter des infos d'accès

---
+++ BadSecApp/Server/Controllers/PubliciteController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BadSecApp.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PubliciteController : Controller
+    {
+        private IConfiguration configuration;
+
+        private PubliciteController(IConfiguration config)
+        {
+            configuration = config;
+        }
+
+        [HttpGet]
+        public Task<string> GetPublicite(int numPub)
+        {
+            using (HttpClient client = new HttpClient() { BaseAddress = new Uri(configuration["ServeurPublicite"]) })
+                return client.GetStringAsync("?page=" + numPub);
+        }
+    }
+}
