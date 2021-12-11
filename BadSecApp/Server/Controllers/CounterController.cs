237a1053839b8c23a355762f04481d85f278b72b
JP Gouigoux <jp.gouigoux@free.fr>
2021-12-11 15:58:14 +0100

    Correction du risque sur la concaténation de chemin (voir https://agirlamonggeeks.com/2020/10/04/c-be-careful-with-path-combine/)

--- BadSecApp/Server/Controllers/CounterController.cs
+++ BadSecApp/Server/Controllers/CounterController.cs
@@ -16,9 +16,7 @@ namespace BadSecApp.Server.Controllers
         public IActionResult Envoyer(string valeur, string path)
         {
             // SECU (A03:2021-Injection) : Suppression du dossier en fonction des parametres - Risque de suppression d'un dossier non souhaité
-            // Point de détail : si valeur contient / alors Path.Combine va se positionner sur la racine du disque et on va essayer de supprimer tout le disque dur
-            // Dans le cas d'une suppression il faut veiller à bien comprendre la difference entre Path.Combine et Path.Join
-            string dossier = Path.Combine(path, valeur);
+            string dossier = Path.Join(path, valeur);
 
             if (Directory.Exists(dossier))
             {
